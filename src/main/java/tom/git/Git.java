e976a4c13b6741e71c75e76a8b0babcf8516bc06
tom <1053238064@qq.com>
2022-06-15 12:09:09 +0800

    Initial commit

---
+++ src/main/java/tom/git/Git.java
@@ -0,0 +1,16 @@
+package tom.git;
+
+/**
+ * @author tom;
+ * @create 2022/6/14 16:43
+ */
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("hello git!!");
+        System.out.println("2hello git!!");
+        System.out.println("3hello git!!");
+        System.out.println("4hello git!!");
+        System.out.println("master git!!");
+        System.out.println("hot-fix!!");
+    }
+}
